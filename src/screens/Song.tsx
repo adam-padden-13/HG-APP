8d24e50693f271b3825f32028590364f497595b1
adam <paddenadam@gmail.com>
2023-05-19 13:55:41 -0400

    changes

--- src/screens/Song.tsx
+++ src/screens/Song.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, StyleSheet } from "react-native";
 import { Audio } from "expo-av";
 import { Sound } from "expo-av/build/Audio";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
@@ -9,6 +9,44 @@ const SongScreen = ({ navigation, route }) => {
   const [sound, setSound] = useState<Sound>();
 
   console.log(song.title);
+
+  const player = new Audio.Sound();
+
+  useEffect(() => {
+    console.log("HIT")
+    async function loadSound() {
+      // Load the sound
+      await player.loadAsync({
+        uri: "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3",
+      });
+    }
+
+    async function unloadSound() {
+      // cleanup
+      await player.unloadAsync();
+    }
+
+    loadSound();
+
+    return () => {
+      unloadSound();
+    };
+  }, []);
+
+  async function onPlay() {
+    try {
+      await player.playAsync();
+      console.log(" Your sound is playing!");
+    } catch (error) {
+      // An error occurred!
+      console.log(error);
+    }
+  }
+
+  async function onStop() {
+    console.log("stop");
+    await player.stopAsync();
+  }
 
   async function playSound() {
     if (song.number === 1) {
@@ -60,8 +98,26 @@ const SongScreen = ({ navigation, route }) => {
       <Text> {song.title}</Text>
       <Button title={"Play Song"} onPress={playSound} />
       <Button title={"Stop Song"} onPress={stopSound} />
+      <>
+    <View style={styles.container}>
+      <Button title="Play Sound" onPress={onPlay} />
     </View>
+        <View style={styles.container}>
+      <Button title="Stop Sound" onPress={onStop} />
+    </View>
+    </>
+    </View>
+    
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 10,
+  },
+});
 
 export default SongScreen;
