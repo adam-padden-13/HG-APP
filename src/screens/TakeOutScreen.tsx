72d2e36760ea314281009de8ee48555c8318796b
adam <paddenadam@gmail.com>
2023-06-10 20:01:54 -0400

    egg game

--- src/screens/TakeOutScreen.tsx
+++ src/screens/TakeOutScreen.tsx
@@ -1,11 +1,18 @@
 import { View } from "react-native";
 import { HeaderText, NormalText } from "../theme/theme";
+import { WebView } from "react-native-webview";
+import Constants from "expo-constants";
 
 const TakeOutScreen = () => {
   return (
-    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
-      <HeaderText>Our Favorite Takeout</HeaderText>
-    </View>
+    <WebView
+      source={{ uri: "https://egggame.org/" }}
+      style={{ marginTop: Constants.statusBarHeight }}
+    />
+    // <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+    //   <HeaderText>Our Favorite Takeout</HeaderText>
+    //   <WebView source={{ uri: "https://egggame.org/" }} />
+    // </View>
   );
 };
 
